36ac6e733a537132c9cd17a383d6d597c0171fc7
nallas <nallas@ftdi.com>
2019-07-15 17:01:18 +0530

    init

---
+++ src/test/java/org/ftd/git/Conflict1.java
@@ -0,0 +1,17 @@
+package org.ftd.git;
+
+public class Conflict1 {
+    
+    private int[] array = {1, 23, 89, 12};
+    
+    public void method1() {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Conflict1 conflict1 = new Conflict1();
+        conflict1.method1();
+    }
+}
