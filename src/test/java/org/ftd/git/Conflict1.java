645fd4ded277377187884622897cf81c39fd00cc
nallas <nallas@ftdi.com>
2019-07-15 20:21:19 +0530

    recreate conflict

--- src/test/java/org/ftd/git/Conflict1.java
+++ src/test/java/org/ftd/git/Conflict1.java
@@ -10,7 +10,7 @@ public class Conflict1 {
             System.out.println(i);
             sum += i;
         }
-        System.out.println("Sum: " + sum);
+        System.out.println("Sum2: " + sum);
     }
 
     public static void main(String[] args) {
