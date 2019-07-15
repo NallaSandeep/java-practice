2affee41ca0d1eb11d1343485d55e75232b57056
nallas <nallas@ftdi.com>
2019-07-15 17:59:53 +0530

    Resolved conflict

--- src/test/java/org/ftd/git/Conflict1.java
+++ src/test/java/org/ftd/git/Conflict1.java
@@ -5,17 +5,10 @@ public class Conflict1 {
     private int[] array = {1, 23, 89, 12, 34};
     
     public void method1() {
-<<<<<<< HEAD
         int sum = 0;
         for (int i: array) {
             System.out.println(i);
             sum += i;
-=======
-    	int sum = 0;
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
-            sum += array[i];
->>>>>>> branch 'master' of https://github.com/NallaSandeep/java-practice
         }
         System.out.println("Sum: " + sum);
     }
