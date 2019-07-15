a47a1391c88a77b5039e91b4a0fdf97dd1662620
nallas <nallas@ftdi.com>
2019-07-15 17:21:38 +0530

    sum

--- src/test/java/org/ftd/git/Conflict1.java
+++ src/test/java/org/ftd/git/Conflict1.java
@@ -5,9 +5,12 @@ public class Conflict1 {
     private int[] array = {1, 23, 89, 12};
     
     public void method1() {
+    	int sum = 0;
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
+            sum += array[i];
         }
+        System.out.println("Sum: " + sum);
     }
 
     public static void main(String[] args) {
